e57798496459ddde72347eeac736449af907a471
Administrator <dreamers.ball66@gmail.com>
2020-05-16 21:41:42 +0900

    修正及び動作確認OK

--- clang/src/BT.c
+++ clang/src/BT.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-
-int main()
-{
-  const char *command01 = "bluetoothctl";
-    // "                 connect 00:19:5D:25:05:E5";
+#include <stdlib.h>
   
-  char *command02 = "connect 00:19:5D:25:05:E5";
+int main(void) {
+  FILE *fp, *outputfile;
+  char ret[40];
 
-  
-  system(command01);
-  printf("%s", command02);
-  // system(command02);
+  fp = popen("echo -e connect 00:19:5D:25:05:E5 | bluetoothctl", "r");
+  while(fgets(ret, sizeof ret, fp) != NULL) {
+    printf("%s", ret);
+  }
+  pclose(fp);
+
+  outputfile = fopen("text.txt", "a");
+  fprintf(outputfile, "%s\n", ret);
+  fclose(outputfile);
   return 0;
-
 }
