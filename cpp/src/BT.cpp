efd8a4c6475f30eba0f0cca438d3a15d0df0b070
Administrator <dreamers.ball66@gmail.com>
2020-05-17 00:32:29 +0900

    修正及び動作確認OK

--- cpp/src/BT.cpp
+++ cpp/src/BT.cpp
@@ -1,13 +1,26 @@
-#if 0
-g++ --std=c++14 "$0" -o ../cpp-BT && ../cpp-BT
-exit
-#endif
-
 #include <iostream>
-#include <fstream>
+#include <cstdlib>
+#include <ext/stdio_filebuf.h>
   
-int main(void) {
-  std::system("bluetoothctl");
-  std::system("connect 00:19:5D:25:05:E5");
+int main() {
+
+  std::string cmd = "echo -e connect 00:19:5D:25:05:E5 | bluetoothctl";
+
+  FILE *fp = popen(cmd.c_str(), "r");
+
+  // streambufを作成し，istreamのコンストラクタに渡す
+  __gnu_cxx::stdio_filebuf<char> *p_fb = new __gnu_cxx::stdio_filebuf<char>(fp, std::ios_base::in);
+  std::istream input(static_cast<std::streambuf *>(p_fb));
+
+  // getlineでストリームからコマンド出力を受け取る
+  std::string buffer;
+  while(getline(input, buffer)){
+    std::cout << "out > " << buffer << std::endl;
+  }
+
+  // 最後に解放
+  delete p_fb;
+  pclose(fp);
+
   return 0;
 }
